d4749560e5c092401948fbddf3b4a03ed1f3173f
Victor Mateo Milan <victor03.vmm@gmail.com>
2024-10-07 13:15:55 +0200

    Version quita de mi codigo con el js en fichero externo

---
+++ ScriptJS.js
@@ -0,0 +1,25 @@
+var numero=Math.trunc(Math.random()*40)+10
+        var cont=0;
+        var suma=0;
+        for(i=2;i<numero;i++){
+            if(esPrimo(i)){
+                document.getElementById("resul").innerHTML+=i+"<br>";
+                suma+=i;
+                console.log(suma)
+                cont++
+            }
+        }
+
+        media=Math.trunc(suma/cont);
+        document.getElementById("contados").innerHTML+="Se han contado: "+cont;
+        document.getElementById("media").innerHTML+="La media es: "+media;
+
+        function esPrimo(numero){
+            let esPrimo=true
+            for(let i=2;i<numero;i++){
+                if(numero%i==0){
+                    esPrimo=false
+                }
+            }
+            return esPrimo
+        }
