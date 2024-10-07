2b65a7f789fbe28880dc78865c4d59b962f26be0
Victor Mateo Milan <victor03.vmm@gmail.com>
2024-10-07 13:52:08 +0200

    Version siete de mi JS ampliacion de comentarios

--- ScriptJS.js
+++ ScriptJS.js
@@ -1,6 +1,7 @@
 var numero=Math.trunc(Math.random()*40)+10
         var cont=0;
         var suma=0;
+        //Bucle 
         for(i=2;i<numero;i++){
             if(esPrimo(i)){
                 document.getElementById("resul").innerHTML+=i+"<br>";
@@ -14,6 +15,7 @@ var numero=Math.trunc(Math.random()*40)+10
         document.getElementById("contados").innerHTML+="Se han contado: "+cont;
         document.getElementById("media").innerHTML+="La media es: "+media;
 
+        //Funcion para ver si el numero es primo
         function esPrimo(numero){
             let esPrimo=true
             for(let i=2;i<numero;i++){
